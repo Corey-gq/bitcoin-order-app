bea59f6c2eb4bfffd2ab90f26e1eb361a198f624
Corey-gq <2792970241@qq.com>
2025-06-05 13:40:14 +0800

    add DAST scan with ZAP

--- scripts/zap-script.sh
+++ scripts/zap-script.sh
@@ -1,3 +1,12 @@
+
+
+#!/bin/bash
+
+docker pull owasp/zap2docker-stable
+docker run -i owasp/zap2docker-stable zap-baseline.py -t "https://corey-gq.github.io/bitcoin-order-app/" -l PASS > zap_baseline_report.html
+
+echo $? > /dev/null
+
 #!/bin/bash
 
 docker pull zaproxy/zap-stable
